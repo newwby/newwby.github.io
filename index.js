a7a12882d35c13868e99326e2e85595b5db9a86d
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2024-12-16 21:05:50 +0000

    dynamic divloading of navbar after dom finished

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-// Hardcoded navigation bar name is not great (TODO reviist)
+// Hardcoded navigation bar name is not great (TODO revise)
 function navAddResponsive() {
   var navbar = document.getElementById("mynavigation_bar");
   if (navbar.className === "navigation_bar") {
@@ -9,23 +9,17 @@ function navAddResponsive() {
   }
 }
 
-// <!-- Loading logic for header (just header atm) -->
-// <!-- TODO move CV page into separate HTML and load that -->
-//  <!-- Landing page should not be CV - it should be a targeted page explaining who I am -->
-function loadNavBar() {
-  // index 0 should be target div, index 1 should be path to html file
-  // TODO fix this doesn't work rn
-  let div_data = [
-    ['header_container', 'header.html'],
-  ]
-
-  for (let data_pair of div_data) {
-    // Dynamically load the header - can use this same logic to load page elements on demand
-    // TODO - this returns a promise and will only load one element as a result, need to validate/store and handle
-    document.getElementById(data_pair[0]).innerHTML = fetch(data_pair[1])
-      .then(response => response.text())
-      .then(data => document.getElementById(data_pair[0]).innerHTML = data)
-      .catch(error => console.error('Error loading:', data_pair));
-    console.log(data_pair, " loaded")
-  }
-}
+function loadDiv(arg_div, arg_page) {
+    // TODO add validation for page existing and both arguments being strings
+    let doc_div_element = document.getElementById(arg_div)
+    // check div element is found
+    if (doc_div_element !== null) {
+        doc_div_element.innerHTML = fetch(arg_page)
+            .then(response => response.text())
+            .then(data => doc_div_element.innerHTML = data)
+            .catch(error => console.error(`Error loading ${arg_div} & ${arg_page}`))
+    }
+    else {
+        console.log('Null error on loadDiv arg_div')
+        }
+    }
