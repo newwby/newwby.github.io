6dcf76d0f9c3e4a0ebc23eade69a01ed5c7a1d33
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2025-01-19 19:45:26 +0000

    PCC Vercel Fetch
    CORS issue, need to fix that

---
+++ tools/pcc_lookup/pcc_main.js
@@ -0,0 +1,39 @@
+
+// make sure the postcode entry field is focused on page load
+function focusOnLoad() {
+    // postcode.focus()
+    document.getElementById("postcode").focus()
+}
+
+// func to take given postcode and return the associated CCG/ICB pair; fires on clicking button (onclick event)
+async function lookupPCC() {
+    // results div is the block where the text content appears after entering postcode
+    resultsDiv.innerHTML = ""; // Clear any previous results
+    
+    // if no postcode prompt user to enter one
+    if (!postcode) {
+        resultsDiv.textContent = "Please enter a postcode.";
+        return;
+    }
+    
+    let pcc_output = false
+    // await so the promise resolves before trying to populate resultsDiv text
+    let fetch_string = 'https://pcc-lookup.vercel.app/home?postcode='+encodeURIComponent(postcode.value)
+    let lookup = await fetch(fetch_string)
+        .then(response => response.json())
+        .then(data => pcc_output = data.result) // true or false
+        .catch(error => console.error('Error:', error));
+
+    resultsDiv.innerHTML = pcc_output
+    
+}
+
+// Bottom of file
+window.onload = focusOnLoad()
+document.getElementById("postcode").addEventListener("keypress", function(event){
+    if (event.key === "Enter") {
+        // prevent any other behaviour from input event then click the lookup button
+        event.preventDefault();
+        document.getElementById("searchButton").click()
+    }
+    })
